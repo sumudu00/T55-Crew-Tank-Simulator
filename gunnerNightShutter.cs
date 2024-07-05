496fab10e72bd9a6abb0c4afd9a372e1d4cdbc7c
sumudu <sumudupriyankara263@gmail.com>
2024-07-05 09:36:33 +0530

    gunner shutter

---
+++ gunnerNightShutter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gunnerNightShutter : MonoBehaviour
+{
+    public RawImage rawImage;
+
+    // Flag to keep track of whether the image is currently enabled or disabled
+    private bool isImageEnabled = true;
+
+    
+
+    void Start()
+    {
+        // Get the RawImage component if not assigned
+        if (rawImage == null)
+        {
+            rawImage = GetComponent<RawImage>();
+        }
+
+        // Ensure the RawImage is initially enabled
+        EnableImage();
+    }
+
+    void Update()
+    {
+        // Check for the "V" key press
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            // Toggle the image visibility
+            if (isImageEnabled)
+                DisableImage();
+            else
+                EnableImage();
+        }
+    }
+
+    // Method to enable the RawImage
+    private void EnableImage()
+    {
+        //rawImage.enabled = true;
+       
+        isImageEnabled = true;
+    }
+
+    // Method to disable the RawImage
+    private void DisableImage()
+    {
+        rawImage.enabled = false;
+        isImageEnabled = false;
+    }
+}
+
+
---
+++ nightSightController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nightSightController : MonoBehaviour
+{
+    private Camera cam;
+    //private bool isNightsightEnabled = false;
+
+    // Two different FOVs for the camera
+    private float fov1 = 6f;
+    private float fov2 = 5.5f;
+
+    // Two different focal lengths for the camera
+    //private float focalLength1 = 219.4331f;
+    //private float focalLength2 = 239.4165f;
+
+    private Texture2D blackTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the Camera component
+        cam = GetComponent<Camera>();
+        //cam.enabled = false;
+
+        //// Create a black texture
+        //blackTexture = new Texture2D(1, 1);
+        //blackTexture.SetPixel(0, 0, Color.black);
+        //blackTexture.Apply();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the 'V' key was pressed
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    // Toggle the Nightsight feature
+        //    isNightsightEnabled = !isNightsightEnabled;
+
+        //    // If Nightsight is enabled, start rendering
+        //    if (isNightsightEnabled)
+        //    {
+        //        cam.enabled = true;
+               
+        //    }
+        //    // If Nightsight is disabled, stop rendering
+        //    else
+        //    {
+        //        cam.enabled = false;
+        //    }
+
+        //    OnGUI();
+
+        //}
+
+
+        // Check if the 'Z' key was pressed
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            // If the current FOV is fov1, change it to fov2, and vice versa
+            if (cam.fieldOfView == fov1)
+            {
+                cam.fieldOfView = fov2;
+                //cam.sensorSize = new Vector2(focalLength2, focalLength2);
+            }
+            else
+            {
+                cam.fieldOfView = fov1;
+                //cam.sensorSize = new Vector2(focalLength1, focalLength1);
+            }
+        }
+    }
+
+
+    //// Draw the black texture when the camera is not rendering
+    //void OnGUI()
+    //{
+    //    if (!cam.enabled)
+    //    {
+    //        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blackTexture);
+    //    }
+    //}
+}
