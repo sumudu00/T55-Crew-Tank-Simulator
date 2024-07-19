1406a790979eb55b09afded9fac6787f1dead15f
sumudu <sumudupriyankara263@gmail.com>
2024-07-19 11:33:42 +0530

    from previous project

---
+++ Delete_Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete_Timer : MonoBehaviour
+{
+    public float lifeTime = 2.0f;
+	
+    void Start()
+    {
+        Destroy(this.gameObject , lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
