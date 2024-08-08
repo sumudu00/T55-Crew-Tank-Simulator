e3dadf105c46b23c35728d57a9392c3b283355c8
sumudu <sumudupriyankara263@gmail.com>
2024-08-08 16:24:10 +0530

    path find for enemy

---
+++ enemyPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPath : MonoBehaviour
+{
+    public Transform[] pathPoints;
+    public int targetPoint;
+    public float speed;
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pathPoints[targetPoint].position)
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pathPoints[targetPoint].position, speed * Time.deltaTime);
+    }
+
+    void increaseTargetInt()
+    {
+        targetPoint++;
+
+        if (targetPoint >= pathPoints.Length )
+        {
+            targetPoint = 0;
+        }
+    }
+}
